0195c4f4dc52dff7ad13d201902b2fd9e14a5782
Edwin Romero <edwinrmrjr@gmail.com>
2024-03-31 02:09:00 +0800

    $_GET

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="get">
+        <label>Username:</label><br>
+        <input type="text" name="username"><br>
+        <label>Password:</label><br>
+        <input type="password" name="password"><br>
+        <input type="submit" value="Login">
+    </form>
+</body>
+</html>
 <?php
-    $name = "edwin";
-    $age = 22;
-    $dollar = 2;
-    echo"my name is {$name} my age is {$age} and i have \${$dollar} in my pocket";
+    echo "{$_GET["username"]} <br>";
+    echo "{$_GET["password"]} <br>";
 ?>
