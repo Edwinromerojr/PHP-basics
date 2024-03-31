ba790ad1ab54c98e5fe11a2a68fec3dcbb7e7613
Edwin Romero <edwinrmrjr@gmail.com>
2024-04-01 02:24:37 +0800

    if statements exercise

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
 <?php
-    $age = 22;
-    if ($age >= 18){
-        echo "You may enter this site";
+    $hours = 40;
+    $rate = 15;
+    $weekly_pay = null;
+
+    if($hours <= 0){
+        $weekly_pay = 0;
     }
-    elseif ($age <= 0){
-        echo "That wasn't a valid age";
+    elseif($hours <= 40){
+        $weekly_pay = $hours * $rate;
     }
     else{
-        echo "You must be 18+ to enter";
+        $weekly_pay = ($rate * 40) + (($hours - 40) * ($rate * 1.5));
     }
+
+    echo"You made \${$weekly_pay} this week";
 ?>
