9347b4c82dae6ec780ff42df6a8707ec88b6591c
Edwin Romero <edwinrmrjr@gmail.com>
2024-04-01 02:08:26 +0800

    if statements

--- index.php
+++ index.php
@@ -1,35 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="index.php" method="post">
-        <label>radius:</label><br>
-        <input type="text" name="radius"><br>
-        <input type="submit" value="calculate">
-    </form>
-</body>
-</html>
 <?php
-    $radius = $_POST["radius"];
-    $circumference = null;
-    $area = null;
-    $volume = null;
-
-    $circumference = 2 * pi() * $radius;
-    $circumference = round($circumference, 2);
-
-    $area = pi() * pow($radius, 2);
-    $area = round($area, 2);
-
-    $volume = 4/3 * pi() * pow($radius, 3);
-    $volume = round($volume, 2);
-
-    echo"Circumference = {$circumference}cm <br>";
-    echo"Area = {$area}cm^2 <br>";
-    echo"Volume = {$volume}cm^3 <br>";
-
+    $age = 22;
+    if ($age >= 18){
+        echo "You may enter this site";
+    }
+    elseif ($age <= 0){
+        echo "That wasn't a valid age";
+    }
+    else{
+        echo "You must be 18+ to enter";
+    }
 ?>
