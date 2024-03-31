dc9da5a1ea9b668a1a51c2cf4713b1ba7a86c021
Edwin Romero <edwinrmrjr@gmail.com>
2024-04-01 01:56:24 +0800

    calculations

--- index.php
+++ index.php
@@ -7,32 +7,29 @@
 </head>
 <body>
     <form action="index.php" method="post">
-        <label>x:</label><br>
-        <input type="text" name="x"><br>
-        <label>y:</label><br>
-        <input type="text" name="y"><br>
-        <label>z:</label><br>
-        <input type="text" name="z"><br>
-        <input type="submit" value="total">
+        <label>radius:</label><br>
+        <input type="text" name="radius"><br>
+        <input type="submit" value="calculate">
     </form>
 </body>
 </html>
 <?php
-    $x = $_POST["x"];
-    $y = $_POST["y"];
-    $z = $_POST["z"];
-    $total = null;
+    $radius = $_POST["radius"];
+    $circumference = null;
+    $area = null;
+    $volume = null;
 
-    // $total = abs($x);
-    // $total = round($x);
-    // $total = floor($x);
-    // $total = ceil($x);
-    // $total = sqrt($x);
-    // $total = pow($x, $y);
-    // $total = max($x, $y, $z);
-    // $total = min($x, $y, $z);
-    // $total = pi();
-    $total = rand();
+    $circumference = 2 * pi() * $radius;
+    $circumference = round($circumference, 2);
 
-    echo $total;
+    $area = pi() * pow($radius, 2);
+    $area = round($area, 2);
+
+    $volume = 4/3 * pi() * pow($radius, 3);
+    $volume = round($volume, 2);
+
+    echo"Circumference = {$circumference}cm <br>";
+    echo"Area = {$area}cm^2 <br>";
+    echo"Volume = {$volume}cm^3 <br>";
+
 ?>
