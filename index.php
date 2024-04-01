eef3caef928415ab85eb6c57f8ac437a7650d5cb
Edwin Romero <edwinrmrjr@gmail.com>
2024-04-02 06:26:45 +0800

    associative arrays exercise

--- index.php
+++ index.php
@@ -1,21 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        <label>Enter a country</label><br>
+        <input type="text" name="country"><br>
+        <input type="submit">
+    </form>
+</body>
+</html>
 <?php
     $capitals = array("USA"=>"Washington D.C.",
                       "Japan"=>"Tokyo",
                       "South Korea"=>"Seoul",
                       "India"=>"New Delhi");
 
-    // $capitals["USA"] = "Las Vegas";
-    // $capitals["China"] = "Beijing";
-    // array_pop($capitals);
-    // array_shift($capitals);
-    // $keys = array_keys($capitals);
-    // $values = array_values($capitals);
-    // $capitals = array_flip($capitals);
-    // $capitals = array_reverse($capitals);
-    echo count($capitals) . "<br>";
+    $capital = $capitals[$_POST["country"]];
+    echo $capital;
 
-    echo $capitals["USA"] . "<br>";
-    foreach($capitals as $key => $value){
-        echo"{$key} = {$value} <br>";
-    }
 ?>
