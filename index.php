e47f02e7687e1a8ae8b8023e189c319983d639d6
Edwin Romero <edwinrmrjr@gmail.com>
2024-04-02 03:12:28 +0800

    while loop

--- index.php
+++ index.php
@@ -1,22 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="index.php" method="post">
-        <label>Enter a number to count to:</label><br>
-        <input type="text" name="counter"><br>
-        <input type="submit" value="start">
-    </form>
-</body>
-</html>
 <?php
-    $counter = $_POST["counter"];
-
-    for($i = 0; $i <= $counter; $i++){
-        echo $i . "<br>";
+    $counter = 0;
+    while($counter < 10){
+        $counter++;
+        echo $counter . "<br>";
     }
 ?>
