31198443a1c45c0d986f351982c0f1b8096261ea
Edwin Romero <edwinrmrjr@gmail.com>
2024-04-02 02:18:06 +0800

    for loop

--- index.php
+++ index.php
@@ -1,23 +1,6 @@
 <?php
-    $grade = "A";
-
-    switch($grade){
-        case "A":
-            echo"You did great";
-            break;
-        case "B":
-            echo"You did good";
-            break;
-        case "C":
-            echo"You did ok";
-            break;
-        case "D":
-            echo"You did poorly";
-            break;
-        case "F":
-            echo"You failed";
-            break;
-        default:
-            echo"{$grade} is not valid";
+    // (counter; condition; inc or dec)
+    for($i = 1;$i < 5; $i++){
+        echo $i . "<br>";
     }
 ?>
