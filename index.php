40fa97e7ef40003faca65640f3f6e492acf5d000
Edwin Romero <edwinrmrjr@gmail.com>
2024-04-02 02:23:45 +0800

    for loop exercise

--- index.php
+++ index.php
@@ -1,6 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        <label>Enter a number to count to:</label><br>
+        <input type="text" name="counter"><br>
+        <input type="submit" value="start">
+    </form>
+</body>
+</html>
 <?php
-    // (counter; condition; inc or dec)
-    for($i = 1;$i < 5; $i++){
+    $counter = $_POST["counter"];
+
+    for($i = 0; $i <= $counter; $i++){
         echo $i . "<br>";
     }
 ?>
