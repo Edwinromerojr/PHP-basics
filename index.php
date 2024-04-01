75fb7269ecbd487ab2b7d99eb4c827aca45a5907
Edwin Romero <edwinrmrjr@gmail.com>
2024-04-02 03:43:06 +0800

    arrays

--- index.php
+++ index.php
@@ -1,26 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="index.php" method="post">
-        <input type="submit" name="stop" value="stop">
-    </form>
-</body>
-</html>
 <?php
-    $seconds = 0;
-    $running = true;
-    while($running){
-        if(isset($_POST["stop"])){
-            $running = false;
-        }
-        else{
-            $seconds++;
-            echo $seconds . "<br>";
-        }
+    $foods = array("apple", "orange", "banana", "coconut");
+    // $foods[0] = "pineapple";
+    // array_push($foods, "pineapple", "kiwi");
+    // array_pop($foods);
+    // array_shift($foods);
+    // $foods = array_reverse($foods);
+    echo count($foods) . "<br>";
+
+    echo $foods[0] . "<br>";
+    foreach($foods as $food){
+        echo $food . "<br>";
     }
 ?>
