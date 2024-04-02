0585298784f79d3428315f78d9d0b8666ce54896
Edwin Romero <edwinrmrjr@gmail.com>
2024-04-03 03:50:07 +0800

    empty() function

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
     // isset() = Returns TRUE if a variable is declared and not null
     // empty() = Returns TRUE if a variable is not declared, false, null, ""
 
-    $username = null;
-    if (isset($username)){
-        echo"This variable is set";
+    $username = "";
+    if (empty($username)){
+        echo"This variable is empty";
     }
     else{
-        echo"This variable is NOT set";
+        echo"This variable is NOT empty";
     }
 ?>
