194dee68c985dd0734af888abcab257d9d863345
Edwin Romero <edwinrmrjr@gmail.com>
2024-04-04 02:24:35 +0800

    functions

--- index.php
+++ index.php
@@ -1,26 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="index.php" method="post">
-        <input type="checkbox" name="foods[]" value="Pizza">Pizza<br>
-        <input type="checkbox" name="foods[]" value="Hamburger">Hamburger<br>
-        <input type="checkbox" name="foods[]" value="Hotdog">Hotdog<br>
-        <input type="checkbox" name="foods[]" value="Donut">Donut<br>
-        <input type="submit" name="submit"><br>
-    </form>
-</body>
-</html>
 <?php
-    if(isset($_POST["submit"])){
-        $foods = $_POST["foods"];
-
-        foreach($foods as $food){
-            echo $food . "<br>";
-        }
+    function birthday($fname, $age){
+        echo"Happy birthday to you <br>";
+        echo"Happy birthday to you <br>";
+        echo"Happy birthday Happy birthday <br>";
+        echo"Happy birthday to {$fname} <br>";
+        echo"You are {$age} years old <br>";
     }
+
+    birthday("Popo", 22);
 ?>
