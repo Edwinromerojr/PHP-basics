a67da0ec9a9185b59e63a9a2eb50eec290baf313
Edwin Romero <edwinrmrjr@gmail.com>
2024-04-04 03:12:52 +0800

    string functions

--- index.php
+++ index.php
@@ -1,8 +1,20 @@
 <?php
-    function hypotenuse($a, $b){
-        $c = sqrt($a ** 2 + $b ** 2);
-        return $c;
-    }
+    $username = array("popo", "the", "great");
+    $phone = "123-123-123";
 
-    echo hypotenuse(4, 5);
+    // $username = strtolower($username);
+    // $username = strtoupper($username);
+    // $username = trim($username);
+    // $username = str_pad($username, 20, "o");
+    // $phone = str_replace("-", "", $phone);
+    // $username = strrev($username);
+    // $username = str_shuffle($username);
+    // $equals = strcmp($username, "Popo popo");
+    // $count = strlen($username);
+    // $index = strpos($username, " ");
+    // $fname = substr($username, 0, 4);
+    // $fullname = explode(" ", $username);
+    $username = implode(" ", $username);
+
+    echo $username;
 ?>
