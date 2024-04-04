920d1aaf223051e5bcfcca7bc19a67058a6e2801
Edwin Romero <edwinrmrjr@gmail.com>
2024-04-05 02:07:48 +0800

    sanitize input

--- index.php
+++ index.php
@@ -1,20 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        username: <br>
+        <input type="text" name="username"><br>
+        <input type="submit" name="login" value="Login"><br>
+    </form>
+</body>
+</html>
 <?php
-    $username = array("popo", "the", "great");
-    $phone = "123-123-123";
-
-    // $username = strtolower($username);
-    // $username = strtoupper($username);
-    // $username = trim($username);
-    // $username = str_pad($username, 20, "o");
-    // $phone = str_replace("-", "", $phone);
-    // $username = strrev($username);
-    // $username = str_shuffle($username);
-    // $equals = strcmp($username, "Popo popo");
-    // $count = strlen($username);
-    // $index = strpos($username, " ");
-    // $fname = substr($username, 0, 4);
-    // $fullname = explode(" ", $username);
-    $username = implode(" ", $username);
-
-    echo $username;
+    if(isset($_POST["login"])){
+        $username = filter_input(INPUT_POST, "username",
+                                FILTER_SANITIZE_SPECIAL_CHARS);
+        echo "Hello {$username}";
+    }
 ?>
