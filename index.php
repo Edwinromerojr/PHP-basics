350eef22a2539678d423e048416f669da6403fcb
Edwin Romero <edwinrmrjr@gmail.com>
2024-04-06 23:39:54 +0800

    COOKIE

--- index.php
+++ index.php
@@ -1,18 +1,17 @@
 <?php
-    include("header.html");
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    This is the Home page <br>
-    Stuff about your home page can go here <br>
-</body>
-</html>
-<?php
-    include("footer.html");
+    // (name, value, expire, filepath)
+    setcookie("fav_food", "donut", time() + (86400 * 2), "/");
+    setcookie("fav_drink", "water", time() + (86400 * 3), "/");
+    setcookie("fav_dessert", "ice cream", time() + (86400 * 4), "/");
+
+    foreach($_COOKIE as $key => $value){
+        echo"{$key} = {$value} <br>";
+    }
+
+    if(isset($_COOKIE["fav_food"])){
+        echo"BUY SOME {$_COOKIE["fav_food"]}!!";
+    }
+    else{
+        echo"i dont know your favorite food";
+    }
 ?>
